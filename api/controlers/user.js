d75838d1e5d2896d65087182707ad0a6cfc4569f
isaias <isaias30silva@gmail.com>
2024-05-07 19:54:11 -0300

    Adicionando arquivos da pasta frontend

---
+++ api/controlers/user.js
@@ -0,0 +1,58 @@
+import { db } from "../db.js";
+
+export const getUsers = (_, res) => {
+    const q = "SELECT * FROM new_table";
+
+    db.query(q, (err, data) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json(data);
+    });
+};
+
+export const addUser = (req, res) => {
+    const q = 
+    "INSERT INTO new_table(`nome`, `endereco`, `fone`, `email`) VALUES(?)";
+
+    const values = [
+        req.body.nome,
+        req.body.endereco,
+        req.body.fone,
+        req.body.email,
+    ];
+
+    db.query(q, [values], (err) => {
+        if(err) return res.json(err);
+
+        return res.status(200).json("Usuário criado com sucesso.");
+    });
+};
+
+export const updateUser = (req, res) => {
+    const q = 
+    "UPDATE new_table SET `nome` = ?, `endereco` = ?, `fone` = ?, `email` = ? WHERE `id` = ?";
+
+    const values = [
+        req.body.nome,
+        req.body.endereco,
+        req.body.fone,
+        req.body.email,
+    ];
+
+    db.query(q, [...values, req.params.id], (err) => {
+        if(err) return res.json(err);
+
+        return res.status(200).json("Usuário atualizado com sucesso.");
+    });
+};
+
+export const deleteUser = (req, res) => {
+    const q = 
+    "DELETE FROM new_table WHERE `id` = ?";
+
+    db.query(q, [req.params.id], (err) => {
+        if(err) return res.json(err);
+
+        return res.status(200).json("Usuário deletado com sucesso.");
+    });
+};
